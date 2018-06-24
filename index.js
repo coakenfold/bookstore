0b4503cd9af376cb0ae1672d74fbc457c387e36d
Chad Oakenfold <web@oakenfold.ca>
2018-06-24 10:48:40 -0700

    Flattened server & client into one (no need for cors); setup static dir; updated npm scripts & travis;

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 /* eslint-disable no-console */
+const PORT = process.env.PORT || 3000
 const spdy = require('spdy')
 const express = require('express')
-const fs = require('fs')
 const path = require('path')
-const PORT = process.env.PORT || 3000
-
-const options = {}
-
-const app = express()
+const fs = require('fs')
 var helmet = require('helmet')
-app.use(helmet())
+
+const options = {
+  key: fs.readFileSync(__dirname + '/src/server/server.key'),
+  cert: fs.readFileSync(__dirname + '/src/server/server.crt'),
+  ca: fs.readFileSync(__dirname + '/src/server/server.csr'),
+}
+const app = express()
+
 app.disable('x-powered-by')
-app.use(express.static(path.join(__dirname, '/src/public')))
-app.get('/', (req, res) => {
-  res.status(200).render('/src/public/index.html')
+app.use(helmet())
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/api', (req, res) => {
+  console.log('Route: /api')
+  res.status(200).json({ status: '/api' })
 })
 
+// Route '/' caught by static dir: build/index.html
+/*app.get('/', (req, res) => {
+  console.log('Route: /')
+  res.sendFile(path.resolve(__dirname, 'src/server/index.html'))
+})*/
+
 spdy.createServer(options, app).listen(PORT, error => {
-  if (error) return process.exit(1)
-  console.log(`!!! Client running at https://localhost:${PORT}`)
+  if (error) {
+    console.error(error)
+    return process.exit(1)
+  } else {
+    console.log(`!!! Client running at https://localhost:${PORT}`)
+  }
 })
