bb031f0d91bc6be1bf7e55450161c3646c62f84d
Chad Oakenfold <web@oakenfold.ca>
2018-06-23 18:22:26 -0700

    Express: moving index.js around, updating package.json

--- server/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => res.send('Express says: Hello World!'))
-
-app.listen(80, () => console.log('Example app listening on port 80!'))
